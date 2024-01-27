846fd4c8956da1dbc0c936eae668cb9afbc37d1a
Christian Johansen <christian.johansen@cpr.ku.dk>
2024-01-27 18:07:27 +0100

    Add tmux config

--- lua/custom/configs/neotree-config.lua
+++ lua/custom/configs/neotree-config.lua
@@ -1,3 +1,20 @@
 vim.api.nvim_exec([[
     autocmd VimEnter * :Neotree
 ]], false)
+
+require("neo-tree").setup({
+        event_handlers = {
+
+          {
+            event = "file_opened",
+            handler = function(file_path)
+              -- auto close
+              -- vimc.cmd("Neotree close")
+              -- OR
+              require("neo-tree.command").execute({ action = "close", source = "filesystem", })
+              require("neo-tree.command").execute({ action = "close", source = "buffers", })
+            end
+          },
+
+        }
+      })
