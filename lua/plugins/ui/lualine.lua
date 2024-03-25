9a5e3e096e61eaa9f25a2686e0b1debbc3a18bfc
Christian Johansen <christian.johansen@cpr.ku.dk>
2024-03-25 11:04:03 +0100

    available buffers manage by lualine

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -2,13 +2,30 @@ return{
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'catppuccin',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
+    config = function ()
+      require('lualine').setup{
+        options = {
+          icons_enabled = true,
+          theme = 'catppuccin',
+          component_separators = '|',
+          section_separators = '',
+        },
+        sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diff', 'diagnostics'},
+            lualine_c = {
+              {
+                'buffers',
+                symbols = {
+                  alternate_file=''
+                },
+              }
+            },
+            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'},
+        }
+      }
+    end
   }
 
