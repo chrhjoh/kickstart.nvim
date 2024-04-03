8f7c49689bc64ef9fc8f43abd598f85a87bb55e4
Christian Johansen <christian.johansen@cpr.ku.dk>
2024-04-03 09:16:44 +0200

    feat(autopairs): prevent autopairing in python import statements
    This commit modifies the autopairing behavior in Python files. Specifically, it prevents autopairing when writing import statements. This change improves the user experience by avoiding unnecessary autopairing in contexts where it's not needed.

--- lua/plugins/editor/autopairs.lua
+++ lua/plugins/editor/autopairs.lua
@@ -12,5 +12,14 @@ return {
       'confirm_done',
       cmp_autopairs.on_confirm_done()
     )
+    local python_handler = cmp_autopairs.filetypes["python"]["("].handler
+    cmp_autopairs.filetypes["python"]["("].handler = function(char, item, bufnr, rules, commit_character)
+      local utils = require('nvim-autopairs.utils')
+      local line = utils.text_get_current_line(bufnr)
+      if line:match("^(from.*import.*)$") then
+        return
+      end
+      python_handler(char, item, bufnr, rules, commit_character)
+    end
   end,
 }
