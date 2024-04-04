b2146a943c6e00c8b608e43e78a14c7945c9cb82
Christian Johansen <christian.johansen@cpr.ku.dk>
2024-04-04 08:57:25 +0200

    fix(nvim-dap): correct typo and remove unused debugger
    - Corrected a typo in the name of the Python launch configuration.
    - Removed 'delve' from the list of ensured installed debuggers as it is not used.

--- lua/plugins/lsp/nvim-dap.lua
+++ lua/plugins/lsp/nvim-dap.lua
@@ -41,7 +41,6 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
       },
     }
 
@@ -87,7 +86,7 @@ return {
       {
         type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
         request = 'launch',
-        name = "Launch file from current workding directory",
+        name = "Launch file from current working directory",
         cwd = vim.fn.getcwd(),
         program = "${file}", -- This configuration will launch the current file if used.
       }
