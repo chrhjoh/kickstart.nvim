0e42ae639f35ad9a47caaa1f03f7cb8a6ebe8fa0
Christian Johansen <christian.johansen@cpr.ku.dk>
2024-04-22 15:28:58 +0200

    change diagnostic loclist severity to WARN

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -10,7 +10,11 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim.keymap.set('n', '<leader>q',
+  function()
+    vim.diagnostic.setloclist({ severity = { min = vim.diagnostic.severity.WARN } })
+  end,
+  { desc = 'Open diagnostics list' })
 
 -- Automatic reselect after indent
 vim.keymap.set("v", "<", "<gv")
@@ -18,8 +22,8 @@ vim.keymap.set("v", ">", ">gv")
 
 vim.keymap.set('n', 'yP', ':let @* = expand("%:p")<CR>', { desc = '[P] Copy Absolute [P]athy' })
 vim.keymap.set('n', 'yp', ':let @* = expand("%")<CR>', { desc = '[p] Copy Relative [P]ath' })
-vim.keymap.set('n', 'y.',  ':let @* = expand("%:t")<CR>', { desc = '[.] Copy Filename' })
+vim.keymap.set('n', 'y.', ':let @* = expand("%:t")<CR>', { desc = '[.] Copy Filename' })
 
 -- Moving between buffers
-vim.keymap.set('n', '<TAB>', ':bn<CR>', {desc='Next Buffer', silent=true})
-vim.keymap.set('n', '<S-TAB>', ':bp<CR>', {desc='Previous Buffer', silent=true})
+vim.keymap.set('n', '<TAB>', ':bn<CR>', { desc = 'Next Buffer', silent = true })
+vim.keymap.set('n', '<S-TAB>', ':bp<CR>', { desc = 'Previous Buffer', silent = true })
