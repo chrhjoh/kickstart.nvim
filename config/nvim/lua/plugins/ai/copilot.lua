1ff3d97a611d2db630021d5109c96c8ac08d952b
Christian Johansen <christian.johansen@cpr.ku.dk>
2024-05-04 17:28:39 +0200

    auto copilot when loaded

--- config/nvim/lua/plugins/ai/copilot.lua
+++ config/nvim/lua/plugins/ai/copilot.lua
@@ -4,7 +4,12 @@ return {
     cmd = "Copilot",
     lazy = true,
     config = function()
-      require("copilot").setup({})
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+        }
+      })
     end,
   },
   {
